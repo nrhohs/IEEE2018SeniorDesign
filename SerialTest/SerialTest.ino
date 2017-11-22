0dfa57c4a7264de19d75206544cf8a3d8d8ea869
nrhohs <33906611+nrhohs@users.noreply.github.com>
2017-11-22 11:25:22 -0600

    Added Arduino Files

---
+++ SerialTest/SerialTest.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(57600); // opens serial port, sets data rate to 57600 baud
+}
+ 
+void loop() {
+  while (Serial.available() > 0) { // if any data available
+    char incomingByte = Serial.read(); // read byte
+    Serial.write(incomingByte); // send it back
+  }
+}
+
