ff41a739fb438291e72688747a8ccbfb18668234
tgonsewski <33811206+tgonsewski@users.noreply.github.com>
2018-01-30 16:00:50 -0600

    Had to change IMU to RTIMU

--- Libraries/navigation.h
+++ Libraries/navigation.h
@@ -11,25 +11,25 @@ int stop_readTOF(TOF *sensor, int display);
 
 void fwd_timed(unsigned char speed, int runtime, int display);
 void fwd_waitOnTOF(unsigned char speed, TOF *sensor, int display, int distance = 110);
-void fwd_waidOnIMU(unsigned char speed, IMU *imu, char axis, double target, int display);
+void fwd_waidOnIMU(unsigned char speed, RTIMU *imu, char axis, double target, int display);
 
 void bwd_timed(unsigned char speed, int runtime, int display);
 void bwd_waitOnTOF(unsigned char speed, TOF *sensor, int display, int distance = 110);
-void bwd_waitOnIMU(unsigned char speed, IMU *imu, char axis, double target, int display);
+void bwd_waitOnIMU(unsigned char speed, RTIMU *imu, char axis, double target, int display);
 
 void strafeRight_timed(unsigned char speed, int runtime, int display);
 void strafeRight_waitOnTOF(unsigned char speed, TOF *sensor, int display, int distance = 110);
-void strafeRight_waitOnIMU(unsigned char speed, IMU *imu, char axis, double target, int display);
+void strafeRight_waitOnIMU(unsigned char speed, RTIMU *imu, char axis, double target, int display);
 
 void strafeLeft_timed(unsigned char speed, int runtime, int display);
 void strafeLeft_waitOnTOF(unsigned char speed, TOF *sensor, int display, int distance = 110);
-void strafeLeft_waitOnIMU(unsigned char speed, IMU *imu, char axis, double target, int display);
+void strafeLeft_waitOnIMU(unsigned char speed, RTIMU *imu, char axis, double target, int display);
 
 void turnRight_timed(unsigned char speed, int runtime, int display);
-void turnRight_waitOnIMU(unsigned char speed, IMU *imu, double targetYaw, int display);
+void turnRight_waitOnIMU(unsigned char speed, RTIMU *imu, double targetYaw, int display);
 
 void turnLeft_timed(unsigned char speed, int runtime, int display);
-void turnLeft_waitOnIMU(unsigned char speed, IMU *imu, double targetYaw, int display);
+void turnLeft_waitOnIMU(unsigned char speed, RTIMU *imu, double targetYaw, int display);
 
-void wallFollowDrive(unsigned char speed, unsigned char direction, TOF *face, TOF *sideOne, TOF *sideTwo, IMU *imu, int display);
-void wallFollowStrafe(unsigned char speed, unsigned char direction, TOF *face, TOF *sideOne, TOF *sideTwo, IMU *imu, int display);
+void wallFollowDrive(unsigned char speed, unsigned char direction, TOF *face, TOF *sideOne, TOF *sideTwo, RTIMU *imu, int display);
+void wallFollowStrafe(unsigned char speed, unsigned char direction, TOF *face, TOF *sideOne, TOF *sideTwo, RTIMU *imu, int display);
