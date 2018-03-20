1c8173ee62b77a2024e6ace0cc64d3a677cf50a2
nrhohs <nrhohs@gmail.com>
2018-03-20 15:39:24 +0000

    Cleaned up Robot3.cpp, Side note, VIM Installed on Robot! 3/20/18 -Will

--- Robot3.cpp
+++ Robot3.cpp
@@ -1,12 +1,12 @@
 /***********************************************************
-library to navigate the course
+  library to navigate the course
 
-Cmdline args:
-"-sensor" does not move the robot but gives a readout of all sensors
-"-route" follwed by a space and a number hardcodes an IR route
-    * Valid routes are 1 - 8.
+  Cmdline args:
+  "-sensor" does not move the robot but gives a readout of all sensors
+  "-route" follwed by a space and a number hardcodes an IR route
+ * Valid routes are 1 - 8.
 
-********************************************************/
+ ********************************************************/
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,38 +26,33 @@ void correctAngle(RTIMU *,int,double);
 
 int main(int argc, char* argv[])
 {
-    //Stop robot on Exit
-    std::atexit(stopExit);
+	//Stop robot on Exit
+	std::atexit(stopExit);
 
-    //    printf("%d, %s, %s", argc, argv[0], argv[1]);
-    //Initialize Stop Switch
-    if(gpioInitialise()<0) return 1;
-    gpioSetMode(21, PI_INPUT);
-    gpioSetPullUpDown(21, PI_PUD_UP);
-//    gpioSetISRFunc(21, FALLING_EDGE, -1, STOPISR);
+	//    printf("%d, %s, %s", argc, argv[0], argv[1]);
+	//Initialize Stop Switch
+	if(gpioInitialise()<0) return 1;
+	gpioSetMode(21, PI_INPUT);
+	gpioSetPullUpDown(21, PI_PUD_UP);
+	//    gpioSetISRFunc(21, FALLING_EDGE, -1, STOPISR);
 
-    //Initializes MUX
-    MUX *mux = initMUX();
+	//Initializes MUX
+	MUX *mux = initMUX();
 
-    //Initializes TOFs
-    //Let's create an array of TOF's so that we can pass them to a function	
-    TOF *tof0 = newTOF(0, mux, 0);	//front facing SR
-    TOF *tof1 = newTOF(0, mux, 1);	//right facing SR (front)
-    TOF *tof2 = newTOF(1, mux, 2);	//right facing SR (back)
-    TOF *tof3 = newTOF(1, mux, 3);	//back facing SR
-    TOF *tof4 = newTOF(0, mux, 4);	//left facing SR  (back)
-    TOF *tof5 = newTOF(1, mux, 5);	//left facing SR  (front)
-    TOF *tof6 = newTOF(1, mux, 6);	//right facing LR (box TOF)
-//    TOF *tof7 = newTOF(1, mux, 7);	//front facing LR (dropoff TOF)
+	//Initializes TOFs
+	TOF *tof0 = newTOF(0, mux, 0);	//front facing SR
+	TOF *tof1 = newTOF(0, mux, 1);	//right facing SR (front)
+	TOF *tof2 = newTOF(1, mux, 2);	//right facing SR (back)
+	TOF *tof3 = newTOF(1, mux, 3);	//back facing SR
+	TOF *tof4 = newTOF(0, mux, 4);	//left facing SR  (back)
+	TOF *tof5 = newTOF(1, mux, 5);	//left facing SR  (front)
+	TOF *tof6 = newTOF(1, mux, 6);	//right facing LR (box TOF)
+	//    TOF *tof7 = newTOF(1, mux, 7);	//front facing LR (dropoff TOF)
 
-    //Initializes IMU
+	//Initializes IMU
 	RTIMU *imu = imuInit();
-    //We can make a robot object that takes in the TOF* array RTIMU* and MUX*
-    //The code directly below would be moved to an InitRobot() function
-    //If initialization occurs correctly a state machine can begin with the Robot
-    //object as the argument to the function	
 
-    //Display Initialization
+	//Display Initialization
 	int display, j;
 	wiringPiSetup();
 	mcp23017Setup (100, 0x20);
@@ -90,104 +85,115 @@ int main(int argc, char* argv[])
 	usleep(2000000);  /* waits 2000ms for stable condition */
 
 	/*****************
-	Actual Robot Start
-	*****************/
+	  Actual Robot Start
+	 *****************/
 
-        int rawcode = 0;
+	int rawcode = 0;
 	//int rawcode=7;
 	//provides sensor readouts
 	if (argc>1){
-	   for (int i=0;i<argc;i++) {
-		if (strcmp(argv[i],"-sensor")==0){
-			while(1){
-				//senor readout code
-				printf("TOF0: %d  ",getDistance(tof0));
-				printf("TOF1: %d  ",getDistance(tof1));
-				printf("TOF2: %d  ",getDistance(tof2));
-				printf("TOF3: %d  ",getDistance(tof3));
-				printf("TOF4: %d  ",getDistance(tof4));
-				printf("TOF5: %d  ",getDistance(tof5));
-				printf("TOF6: %d  ",getDistance(tof6));
-				printf("IMU:  %f\n",getCurrImuYaw(imu));
+		for (int i=0;i<argc;i++) {
+			//Repeatedly display sensor readings, no motor functionality
+			if (strcmp(argv[i],"-sensor")==0){
+				while(1){
+					//senor readout code
+					printf("TOF0: %d  ",getDistance(tof0));
+					printf("TOF1: %d  ",getDistance(tof1));
+					printf("TOF2: %d  ",getDistance(tof2));
+					printf("TOF3: %d  ",getDistance(tof3));
+					printf("TOF4: %d  ",getDistance(tof4));
+					printf("TOF5: %d  ",getDistance(tof5));
+					printf("TOF6: %d  ",getDistance(tof6));
+					printf("IMU:  %f\n",getCurrImuYaw(imu));
+				}
+			}
+			//Hardcode the route from user input
+			else if (strcmp(argv[i],"-route")==0){
+				rawcode=argv[i+1][0]-'0';
+			}
+
+			//Currently (3/20/2018) being used as a debugging flag
+			//for the latter stages of Robot testing
+			else if (strcmp(argv[i],"-pid")==0){
+				strafeRight_waitOnTOF(60,tof3,display,200);
+				stop(50000,display);
+				if(getCurrImuYaw(imu)<180)
+					turnLeft_waitOnIMU(20,imu,0.0,display,true);
+				else
+					turnRight_waitOnIMU(20,imu,0.0,display,true);
+				stop(50000,display);
+				fwd_waitTOFwIMU(25,imu,0.0,tof5,300,display);
+				bwd_waitTOFwIMU(10,imu,0.0,tof6,200,display);
+				stop(50000,display);
+				strafeRight_waitOnTOF(30,tof3,display,290);
+				stop(50000,display);
+				return 0;
+
+				/*
+				   stop(50000,display);
+				   strafeRight_waitOnTOF(30,tof4,display,60);
+				   if(getCurrImuYaw(imu)<180)
+				   turnLeft_waitOnIMU(20,imu,0.0,display,true);
+				   else
+				   turnRight_waitOnIMU(20,imu,0.0,display,true);
+
+				   fwd_waitOnTOF(60,tof5,display,200);
+				   strafeRight_waitOnTOF(30,tof3,display,180);
+
+				   if(getCurrImuYaw(imu)<180)
+				   turnLeft_waitOnIMU(20,imu,0.0,display,true);
+				   else
+				   turnRight_waitOnIMU(20,imu,0.0,display,true);
+				   bwd_waitTOFwIMU(10,imu,0.0,tof6,200,display);
+
+				   return 0;
+				   while(1) {
+				   strafeRight_waitOnTOF(30,tof6,display,300);
+				   stop(50000,display);
+				   strafeLeft_waitOnTOF(30,tof4,display,100);
+				   stop(50000,display);
+				   }
+				   */
 			}
 		}
-		else if (strcmp(argv[i],"-route")==0){
-			rawcode=argv[i+1][0]-'0';
-		}
-		else if (strcmp(argv[i],"-pid")==0){
-			strafeRight_waitOnTOF(60,tof3,display,200);
-			stop(50000,display);
-       			if(getCurrImuYaw(imu)<180)
-  				turnLeft_waitOnIMU(20,imu,0.0,display,true);
-       			else
-  				turnRight_waitOnIMU(20,imu,0.0,display,true);
-			stop(50000,display);
-			fwd_waitTOFwIMU(25,imu,0.0,tof5,300,display);
-			bwd_waitTOFwIMU(10,imu,0.0,tof6,200,display);
-			stop(50000,display);
-			strafeRight_waitOnTOF(30,tof3,display,290);
-			stop(50000,display);
-			return 0;
-
-			/*
-			stop(50000,display);
-			strafeRight_waitOnTOF(30,tof4,display,60);
-       			if(getCurrImuYaw(imu)<180)
-  				turnLeft_waitOnIMU(20,imu,0.0,display,true);
-       			else
-  				turnRight_waitOnIMU(20,imu,0.0,display,true);
-	
-			fwd_waitOnTOF(60,tof5,display,200);
-			strafeRight_waitOnTOF(30,tof3,display,180);
-
-       			if(getCurrImuYaw(imu)<180)
-  				turnLeft_waitOnIMU(20,imu,0.0,display,true);
-       			else
-  				turnRight_waitOnIMU(20,imu,0.0,display,true);
-			bwd_waitTOFwIMU(10,imu,0.0,tof6,200,display);
-
-			return 0;
-			while(1) {
-				strafeRight_waitOnTOF(30,tof6,display,300);
-				stop(50000,display);
-				strafeLeft_waitOnTOF(30,tof4,display,100);
-				stop(50000,display);
-			}
-			*/
-		}
-	    }
 	}
+
+	//Retrieve route information from IRBlaster 
 	else {
 		rawcode = routeread(display);
 	}
 
-				printf("TOF0: %d  ",getDistance(tof0));
-				printf("TOF1: %d  ",getDistance(tof1));
-				printf("TOF2: %d  ",getDistance(tof2));
-				printf("TOF3: %d  ",getDistance(tof3));
-				printf("TOF4: %d  ",getDistance(tof4));
-				printf("TOF5: %d  ",getDistance(tof5));
-				printf("TOF6: %d  ",getDistance(tof6));
-				printf("IMU:  %f\n",getCurrImuYaw(imu));
+	//Display initial Sensor Outputs
+	printf("TOF0: %d  ",getDistance(tof0));
+	printf("TOF1: %d  ",getDistance(tof1));
+	printf("TOF2: %d  ",getDistance(tof2));
+	printf("TOF3: %d  ",getDistance(tof3));
+	printf("TOF4: %d  ",getDistance(tof4));
+	printf("TOF5: %d  ",getDistance(tof5));
+	printf("TOF6: %d  ",getDistance(tof6));
+	printf("IMU:  %f\n",getCurrImuYaw(imu));
 
 	stop(50000,display);
 
+/*----------------------First Task, Go to Button on Top of Ramp--------------------*/	
 	if (rawcode < 4){
-            printf("run A");
-            fwd_waitOnTOF(75, tof5, display, 85);
-	    stop(1000000,display);
-	    bwd_waitOnTOF(75, tof5, display, 400 );
+		printf("run A");
+		fwd_waitOnTOF(75, tof5, display, 85);
+		stop(1000000,display);
+		bwd_waitOnTOF(75, tof5, display, 400 );
 	}
 	else{
-	    printf("run B");
-	    bwd_waitOnTOF(75,tof2,display,85);
-	    stop(1000000,display);
-	    fwd_waitOnTOF(75, tof2, display, 400);
+		printf("run B");
+		bwd_waitOnTOF(75,tof2,display,85);
+		stop(1000000,display);
+		fwd_waitOnTOF(75, tof2, display, 400);
 	}
 
 
+/*----------------------First Task, Go to Button on Top of Ramp--------------------*/	
+
+/*------------Second Task, Go Down Ramp, Then Press button on main arena----------*/	
 	//Rotate 90 to go down ramp
-	//turnLeft_waitOnIMU(25,imu,90.0,display);
 	turnLeft_waitOnIMU(75,imu,80.0,display,false);
 	stop(500000,display);
 
@@ -195,45 +201,46 @@ int main(int argc, char* argv[])
 	fwd_timed(30,5000000,display);
 	stop(1000000,display);
 
-       	if(getCurrImuYaw(imu)>270)
-  		turnLeft_waitOnIMU(20,imu,270.0,display,true);
-       	else
-  		turnRight_waitOnIMU(20,imu,270.0,display,true);
+	//Reorient the Robot 
+	if(getCurrImuYaw(imu)>270)
+		turnLeft_waitOnIMU(20,imu,270.0,display,true);
+	else
+		turnRight_waitOnIMU(20,imu,270.0,display,true);
 
-	//Find box
+	//Go forward until box is found
 	fwd_waitOnTOF(75,tof5,display,400);
 	stop(500000,display);
 
 
 	//Rotate 90 to orient flag mech
 	turnRight_waitOnIMU(75,imu,0.0,display,true);
-        if(getCurrImuYaw(imu)<180)
-	  turnLeft_waitOnIMU(30,imu,0.0,display,true);
-        else
-	  turnRight_waitOnIMU(30,imu,0.0,display,true);
+	if(getCurrImuYaw(imu)<180)
+		turnLeft_waitOnIMU(30,imu,0.0,display,true);
+	else
+		turnRight_waitOnIMU(30,imu,0.0,display,true);
 	if (rawcode%4 < 2){
-	//B button 0
-	/************ Destination B *************************/
+		//B button 0
+		/************ Destination B *************************/
 		bwd_waitOnTOF(50,tof2,display,160);
 		stop(1000000,display);
 		fwd_timed(40,800000,display);
 		stop(1000000,display);
-       		if(getCurrImuYaw(imu)<180)
-	  		turnLeft_waitOnIMU(20,imu,0.0,display,true);
-        	else
-	  		turnRight_waitOnIMU(20,imu,0.0,display,true);
+		if(getCurrImuYaw(imu)<180)
+			turnLeft_waitOnIMU(20,imu,0.0,display,true);
+		else
+			turnRight_waitOnIMU(20,imu,0.0,display,true);
 
-	/***************** Flag Wheel **************************/
+		/***************** Flag Wheel **************************/
 
 		strafeLeft_waitOnTOF(100,tof4,display,100);
 		stop(1000000,display);
 
-       		if(getCurrImuYaw(imu)<180)
-	  		turnLeft_waitOnIMU(20,imu,0.0,display,true);
-        	else
-	  		turnRight_waitOnIMU(20,imu,0.0,display,true);
-	//	if(getDistance(tof2)>60)
-	//		bwd_waitOnTOF(60,tof2,display,60);
+		if(getCurrImuYaw(imu)<180)
+			turnLeft_waitOnIMU(20,imu,0.0,display,true);
+		else
+			turnRight_waitOnIMU(20,imu,0.0,display,true);
+		//	if(getDistance(tof2)>60)
+		//		bwd_waitOnTOF(60,tof2,display,60);
 		stop(100000,display);
 
 		//fwd_waitOnTOF(5,tof0,display,100);
@@ -246,24 +253,24 @@ int main(int argc, char* argv[])
 		sendCommand(22,50,display);
 	}
 	else{
-	//B button 1
-	/****************** Destination B ***********************/
+		//B button 1
+		/****************** Destination B ***********************/
 		fwd_waitOnTOF(75,tof5,display,150);
 		stop(500000,display);
 		bwd_timed(40,800000,display);
 		stop(1000000,display);
-       		if(getCurrImuYaw(imu)<180)
-	  		turnLeft_waitOnIMU(40,imu,0.0,display,true);
-        	else
-	  		turnRight_waitOnIMU(40,imu,0.0,display,true);
+		if(getCurrImuYaw(imu)<180)
+			turnLeft_waitOnIMU(40,imu,0.0,display,true);
+		else
+			turnRight_waitOnIMU(40,imu,0.0,display,true);
 
-	/***************** Flag Wheel **************************/
+		/***************** Flag Wheel **************************/
 		strafeLeft_waitOnTOF(100,tof4,display,100);
 		stop(1000000,display);
-       		if(getCurrImuYaw(imu)<180)
-	  		turnLeft_waitOnIMU(20,imu,0.0,display,true);
-        	else
-	  		turnRight_waitOnIMU(20,imu,0.0,display,true);
+		if(getCurrImuYaw(imu)<180)
+			turnLeft_waitOnIMU(20,imu,0.0,display,true);
+		else
+			turnRight_waitOnIMU(20,imu,0.0,display,true);
 		stop(100000,display);
 
 		//bwd_waitOnTOF(5,tof1,display,100);
@@ -276,16 +283,22 @@ int main(int argc, char* argv[])
 		stop(500000,display);
 		sendCommand(22,50,display);
 	}
+/*--------------------------Task 4 Pick up Treasure Chest------------------------*/
+	
+	strafeRight_waitOnTOF(60,tof4,display,40); //Back off of the wall
+	
+	//Correct Robot Orientation
+	if(getCurrImuYaw(imu)<180)
+		turnLeft_waitOnIMU(20,imu,0.0,display,true);
+	else
+		turnRight_waitOnIMU(20,imu,0.0,display,true);
 
-	strafeRight_waitOnTOF(60,tof4,display,40);
-       	if(getCurrImuYaw(imu)<180)
-  		turnLeft_waitOnIMU(20,imu,0.0,display,true);
-       	else
-  		turnRight_waitOnIMU(20,imu,0.0,display,true);
 	
 	bwd_waitOnTOF(15,tof2,display,420);
 	strafeRight_waitOnTOF(60,tof6,display,50);
-	
+
+/*--------------------------Task 4 Pick up Treasure Chest------------------------*/
+
 	//for future purposes
 	sendCommand(0,0,display);
 
